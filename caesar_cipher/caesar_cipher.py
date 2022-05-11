636744cc93f9b62ca85ceb98a4623aed2a92d222
Eden Brekke <eden.lorrai@gmail.com>
2022-05-11 14:43:32 -0700

    finished decrypt function, working on encrypt

--- caesar_cipher/caesar_cipher.py
+++ caesar_cipher/caesar_cipher.py
@@ -1,10 +1,14 @@
+import re
 from corpus_loader import word_list, name_list
 
-def encrypt():
-  pass
+def encrypt(phrase, shift):
+  # okay, So I know I imported RE and I know that it's probably smarter to use RE but I'm gonna brute force my way through this. 
+  normal_alphabet_lower = "abcdefghijklmnopqrstuvwxyz"
+  normal_alphabet_upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+  
 
-def decrypt():
-  pass
+def decrypt(encrypted_text, key):
+  return encrypt(encrypted_text, -key)
 
-def crack():
+def crack(encrypted_text):
   pass
